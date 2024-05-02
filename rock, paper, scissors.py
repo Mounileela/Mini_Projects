1bdb2b68cf5a7ee9ba731164d7ed42c726b7350e
Mounileela <124118378+Mounileela@users.noreply.github.com>
2024-05-02 23:38:02 +0530

    Added tie message if no one wins.

--- rock, paper, scissors.py
+++ rock, paper, scissors.py
@@ -30,6 +30,9 @@ while True:
         print("You Won!")
         user_wins += 1
 
+    elif user_input == computer_pick:
+        print("TIE")
+
     else:
         print("You lost!")
         computer_wins += 1
